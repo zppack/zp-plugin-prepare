23bd8257dcf77a0420b40fa202953140ca69b70e
shallinta <740159135@qq.com>
2021-06-28 20:54:34 +0800

    feat: add npm prepare plugin

---
+++ README.md
@@ -0,0 +1,17 @@
+# @zppack/zp-plugin-prepare
+
+A plugin for zp to run npm scripts prepare.
+
+Will just run `npm run prepare` if it exists in "npm scripts".
+
+## Contributing
+
+[How to contribute to this?](CONTRIBUTING.md)
+
+## Recently changes
+
+See the [change log](CHANGELOG.md).
+
+## License
+
+[MIT](LICENSE)
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { execSync } from 'child_process';
+import path from 'path';
+import chalk from 'chalk';
+import log from '@zppack/log';
+
+const plugin = async (ctx) => {
+  const { appPath } = ctx;
+
+  const shellCwd = appPath;
+  log.d('Zp Prepare Plugin: shell working directory = ' + chalk.underline(shellCwd));
+
+  const pkg = await import(path.resolve(shellCwd, 'package.json'));
+  log.d('Zp Prepare Plugin: reading package.json: \n', chalk.gray(JSON.stringify(pkg)));
+
+  if (pkg.scripts && pkg.scripts.prepare) {
+  const sh = 'npm run prepare';
+    log.i('Zp Prepare Plugin: npm preparing...');
+    log.d('Zp Prepare Plugin: executing shell ' + chalk.blue(sh));
+    try {
+      execSync(sh, shellCwd);
+      log.i('Zp Prepare Plugin: npm prepared successfully.');
+    } catch (err) {
+      log.e(chalk.redBright(`${err} (Error Code: 3002)`));
+    }
+  }
+};
+
+export default plugin;
